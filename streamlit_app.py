77f163ccd0946e564fdb108c556a0f49ae81de4c
Matthew <plummerfernandez@gmail.com>
2025-04-28 17:06:50 +0000

    css fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,14 @@ import numpy as np
 import random
 import os
 
-# --- custom CSS to make header and footer invisible
 st.markdown(
     """
     <style>
-    footer {visibility:hidden;}
+    footer {display: none;}
     </style>
-    """
+    """,
+    unsafe_allow_html=True
 )
-
 # --- Your custom function for image enhancement ---
 def enhance_image(image, contrast_factor=1.5, black_point=20):
     from PIL import ImageEnhance
