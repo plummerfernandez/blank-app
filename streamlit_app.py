33ad9ff5e51caed3dbd38c35bb844d4982fc9ee7
Matthew <plummerfernandez@gmail.com>
2025-05-09 13:02:24 +0100

    trying quick fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,5 +232,5 @@ if manual_trigger:
 # --- Idle Mode ---
 if should_run_idle():
     st.write("Idle mode triggered!")  # Debugging (remove after testing)
-    #process_image()
+    process_image()
     st.rerun()  # Refresh the app after processing the image in idle mode
